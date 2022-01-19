e088a14bf503b639ed30adc1ee72c683e3c1e706
Cristian Spiridon <cristian_spiridon@discovery.com>
2022-01-19 23:38:50 +0000

    remove pods

--- Pods/Target Support Files/Pods-MTBLogger-MTBLoggerUITests/Pods-MTBLogger-MTBLoggerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MTBLogger_MTBLoggerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MTBLogger_MTBLoggerUITests
-@end
--- Pods/Target Support Files/Pods-MTBLogger/Pods-MTBLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MTBLogger : NSObject
-@end
-@implementation PodsDummy_Pods_MTBLogger
-@end
